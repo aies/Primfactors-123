1c50b18c7a123fdf02ddd85bae476fb786956c7a
ies.cho <ies.cho@sec.com>
2025-07-23 09:56:31 +0900

    [Prodection]p45

--- Primfactors/prim-factors.cpp
+++ Primfactors/prim-factors.cpp
@@ -5,6 +5,8 @@ class PrimeFactors {
 public :
     vector<int> of(int i) {
         vector<int> result = {};
+
+        if (i == 2) result.push_back(2);
         return result;
     }
 
--- Primfactors/prim-factors_test.cpp
+++ Primfactors/prim-factors_test.cpp
@@ -1,9 +1,17 @@
 #include "gmock/gmock.h"
 #include "prim-factors.cpp"
 
-TEST(TC, TC1) {
+TEST(TC, OF1) {
     PrimeFactors prime_factor;
     vector<int> expected = {};
 
     EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+
+TEST(TC, OF2) {
+    PrimeFactors prime_factor;
+    vector<int> expected = {2};
+
+    EXPECT_EQ(expected, prime_factor.of(2));
 }
