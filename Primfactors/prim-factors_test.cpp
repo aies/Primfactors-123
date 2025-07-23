094c23f3c00f3a70c53e03b2aa1733300cd8a7c3
ies.cho <ies.cho@sec.com>
2025-07-23 10:04:22 +0900

    [Prodection]p52

--- Primfactors/prim-factors_test.cpp
+++ Primfactors/prim-factors_test.cpp
@@ -27,3 +27,10 @@ TEST_F(PrimeFixture, OF3) {
 
     EXPECT_EQ(expected, prime_factor.of(3));
 }
+
+TEST_F(PrimeFixture, OF4) {
+    PrimeFactors prime_factor;
+    vector<int> expected = { 2, 2 };
+
+    EXPECT_EQ(expected, prime_factor.of(4));
+}
